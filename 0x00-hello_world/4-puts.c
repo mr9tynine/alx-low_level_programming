27d97ec676cdd77f88747f2faf6a1f360ec62d5e
mr9tynine <wizyokoh@gmail.com>
2023-10-08 08:22:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C program that prints a sentence and a new line
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
